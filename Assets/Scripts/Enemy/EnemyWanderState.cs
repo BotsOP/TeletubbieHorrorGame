26f8bd4675c6ae2ee9df12239ccb8dac6adc140e
dvorkod <denis.dvorkovyy@hva.nl>
2021-09-27 15:47:39 +0200

    Enemy now follows patrol instead of wander

--- Assets/Scripts/Enemy/EnemyWanderState.cs
+++ Assets/Scripts/Enemy/EnemyWanderState.cs
@@ -4,25 +4,100 @@ using UnityEngine;
 
 public class EnemyWanderState : EnemyBaseState
 {
+    private const float SWITCH_PROBABILITY = 0.1f;
+    private const bool WAIT_AT_POINTS = true;
+    private const float PATROL_WAITING_TIME = 5f;
+
     EnemyStateManager enemy;
+
+    private bool patrolForward;
+    private bool walking;
+    private bool waiting;
+    private float startTime;
+    private int currentPatrolIndex;
+
     public override void EnterState(EnemyStateManager enemy)
     {
         this.enemy = enemy;
-        SetDestination();
-        enemy.anim.SetInteger("battle", 1);
+        SetNewDestination();
         enemy.anim.SetInteger("moving", 1);
     }
+
     public override void UpdateState()
     {
-        if(enemy.agent.remainingDistance < 1f)
+        if (enemy.agent.remainingDistance < 0.01f)
         {
-            SetDestination();
+            if (walking)
+            {
+                walking = false;
+                
+
+                if (WAIT_AT_POINTS)
+                {
+                    enemy.anim.SetInteger("moving", 0);
+                    waiting = true;
+                    startTime = Time.time;
+                }
+                else
+                {
+                    ChangePatrolPoint();
+                    SetNewDestination();
+                }
+            }
+
+            SmoothRotation();
+        }
+        else
+        {
+
+        }
+
+        if (waiting && Time.time - startTime > PATROL_WAITING_TIME)
+        {
+            waiting = false;
+
+            ChangePatrolPoint();
+            SetNewDestination();
         }
     }
 
-    private void SetDestination()
+    private void SetNewDestination()
     {
-        Vector3 randomPos = new Vector3(Random.Range(-3f, 3f), 0, Random.Range(-3f, 3f));
-        enemy.agent.SetDestination(randomPos);
+        Vector3 walkTo = enemy.patrolPoints[currentPatrolIndex].position;
+        enemy.agent.SetDestination(walkTo);
+        walking = true;
+        enemy.anim.SetInteger("moving", 1);
+    }
+
+    private void ChangePatrolPoint()
+    {
+        float randomNumber = Random.Range(0f, 1f);
+        if (randomNumber <= SWITCH_PROBABILITY)
+        {
+            patrolForward = !patrolForward;
+        }
+
+        if (patrolForward)
+        {
+            currentPatrolIndex = (currentPatrolIndex + 1) % enemy.patrolPoints.Length;
+        }
+        else
+        {
+            currentPatrolIndex--;
+            if (currentPatrolIndex < 0)
+            {
+                currentPatrolIndex = enemy.patrolPoints.Length - 1;
+            }
+        }
+    }
+
+    private void SmoothRotation()
+    {
+        float rotationSpeed = 15;
+        Vector3 newAngle;
+        newAngle.x = Mathf.LerpAngle(enemy.enemyGameobject.transform.eulerAngles.x, enemy.patrolPoints[currentPatrolIndex].eulerAngles.x, (Time.time - startTime) / rotationSpeed);
+        newAngle.y = Mathf.LerpAngle(enemy.enemyGameobject.transform.eulerAngles.y, enemy.patrolPoints[currentPatrolIndex].eulerAngles.y, (Time.time - startTime) / rotationSpeed);
+        newAngle.z = Mathf.LerpAngle(enemy.enemyGameobject.transform.eulerAngles.z, enemy.patrolPoints[currentPatrolIndex].eulerAngles.z, (Time.time - startTime) / rotationSpeed);
+        enemy.enemyGameobject.transform.eulerAngles = newAngle;
     }
 }
